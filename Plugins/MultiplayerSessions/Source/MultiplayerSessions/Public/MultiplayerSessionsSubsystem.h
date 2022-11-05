45b0c526518a71bde2e3b379459e1fa6fe39cfe5
Dylan Malla <51140000+D-Malla@users.noreply.github.com>
2022-11-04 20:14:16 -0700

    Started functionality for Online Sessions plugin
    - Added functions to handle session functionality.
    - Added Delegates.
    - Added Callback functions.
    - Added Delegate handles.

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "OnlineSessionSettings.h"
 #include "Interfaces/OnlineSessionInterface.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "MultiplayerSessionsSubsystem.generated.h"
@@ -18,8 +19,52 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstan
 public:
 	UMultiplayerSessionsSubsystem();
 
+	/*
+	 * To handle session functionality. The Menu class will call these
+	 */
+
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSessions(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+	void DestroySession();
+	void StartSession();
+
+	/*
+	 * End
+	 */
 protected:
+	/*
+	 * Internal callbacks for the delegates we'll add to the OnlineSessionInterface delegate list.
+	 * This doesn't need to be called outside this class.
+	 */
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
+	
 
 private:
 	IOnlineSessionPtr SessionInterface;
+
+	/*
+	 * To add to the OnlineSessionInterface delegate list.
+	 * We'll bind our MultiplayerSessionsSubsystem internal callbacks to these.
+	 */
+
+	// Session Delegates
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+
+	// Session Delegate Handles
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
+
 };
